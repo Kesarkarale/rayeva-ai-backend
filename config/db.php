216468eec962963cbc02353f51127dafebec4c5f
Kesar Karale <kesarkarale1@gmail.com>
2026-03-06 20:23:17 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,7 +1,14 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "rayeva_ai");
+  <?php
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$host="localhost";
+$user="root";
+$password="";
+$database="rayeva_ai";
+
+$conn=new mysqli($host,$user,$password,$database);
+
+if($conn->connect_error){
+die("Database Connection Failed: ".$conn->connect_error);
 }
+
 ?>
